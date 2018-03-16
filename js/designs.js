2096c347bc818e4e56f799e3ce595a333399dacc
kryaksy <ib.koray@gmail.com>
2018-03-16 14:26:27 +0300

    added fontawesome item array

--- js/designs.js
+++ js/designs.js
@@ -5,6 +5,9 @@ var cards = document.querySelector('.cards');
 /* Converting HTML Collection to an array*/
 var cardsArray = Array.prototype.slice.call(card);
 
+/* Font Awesome LISTENERS*/
+var faItems = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
+
 /* TODO: Display the cards on the page
 - shuffle the list of cards
 - loop through each card and create its HTML
@@ -14,76 +17,76 @@ var cardsArray = Array.prototype.slice.call(card);
 // FUNCTIONS
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var i = array.length,
-    temp,
-    j;
+	var i = array.length,
+		temp,
+		j;
 
-  while (i !== 0) {
-    j = Math.floor(Math.random() * i);
-    i -= 1;
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
+	while (i !== 0) {
+		j = Math.floor(Math.random() * i);
+		i -= 1;
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
 
-  return array;
+	return array;
 }
 
 function showCard(a) {
-  // display the card's symbol
-  a.classList.add('open');
+	// display the card's symbol
+	a.classList.add('open');
 }
 
 function addCard() {
-  //add the card to a *list* of "open" cards
-  console.log('addCard');
-  if (true /*list has two card*/ ) {
-    checkCards();
-  }
+	//add the card to a *list* of "open" cards
+	console.log('addCard');
+	if (true /*list has two card*/ ) {
+		checkCards();
+	}
 
 }
 
 function lockCards() {
-  // function to lock the cards in the open position
-  console.log('lockCards');
+	// function to lock the cards in the open position
+	console.log('lockCards');
 }
 
 function hideCards() {
-  // function to remove the cards from the list and hide the card's symbol
-  console.log('hideCards');
+	// function to remove the cards from the list and hide the card's symbol
+	console.log('hideCards');
 }
 
 function incrementMoveCount() {
-  // function to increment the move counter and display it on the page
-  console.log('incrementMoveCount');
+	// function to increment the move counter and display it on the page
+	console.log('incrementMoveCount');
 }
 
 function endGame() {
-  // function to display a message with the final score
-  console.log('endGame');
+	// function to display a message with the final score
+	console.log('endGame');
 }
 
 function checkCards(A, B) {
-  // check to see if the two cards match
-  if (A === B) {
+	// check to see if the two cards match
+	if (A === B) {
 
-    lockCards();
+		lockCards();
 
-  } else {
+	} else {
 
-    hideCards();
-    incrementMoveCount();
+		hideCards();
+		incrementMoveCount();
 
-  }
+	}
 
 }
 
 function checkIfEnd() {
-  if (false /*all open*/ ) {
+	if (false /*all open*/ ) {
 
-    endGame();
+		endGame();
 
-  }
+	}
 }
 
 
@@ -91,11 +94,11 @@ function checkIfEnd() {
 // TODO: set up the event listener for a card.
 document.querySelector('.cards').addEventListener('click', function(e) {
 
-  showCard(e.target);
+	showCard(e.target);
 
-  addCard();
+	addCard();
 
-  checkIfEnd();
+	checkIfEnd();
 
 });
 
